608e0dad68a37f1606e88c6b926447ef42c1e9cd
HBF <160134222@QQ.COM>
2019-12-04 19:51:18 +0800

    把本地工程提交到git

---
+++ git_itcast/src/cn/itcast/git/GitTest.java
@@ -0,0 +1,10 @@
+package cn.itcast.git;
+
+public class GitTest {
+    public  static void main(String [] args){
+        System.out.println("aaaaa");
+        System.out.println("bbbbbbbb");
+        System.out.println("cccccccccc");
+        System.out.println("ddddddddddd");
+    }
+}
